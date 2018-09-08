22ed5ca0aed9babd233603030e5d36f0e6fe2aba
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-09-08 13:39:07 +0200

    Add styles to TasksList compnent and fix styles in TasksListContainer

--- src/components/TasksListContainer.js
+++ src/components/TasksListContainer.js
@@ -4,9 +4,11 @@ import TasksList from './TasksList';
 const TasksListContainer = () => {
 	return (
 		<div className="tasks-list-container">
-			<h2 className="tasks-list__title">
+			<h2 className="tasks-list-container__title">
 				lista zadań{' '}
-				<strong className="task-list__title--yellow">do zrobienia</strong>
+				<strong className="tasks-list-container__title--yellow">
+					do zrobienia
+				</strong>
 			</h2>
 			<TasksList />
 		</div>
