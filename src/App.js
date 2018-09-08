260424375f6e49dd817cfdb9331c45ab1854342e
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-09-08 09:34:49 +0200

    Add main app container: TodoContainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import TodoContainer from './components/TodoContainer';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<h1 className="title">lista zadań</h1>
+				<TodoContainer />
 			</div>
 		);
 	}
---
+++ src/components/TodoContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TodoContainer = () => {
+	return <div className="todo-container" />;
+};
+
+export default TodoContainer;
