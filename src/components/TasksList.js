2a0b4df3e4a0b55f97a860b31316318bda9fbb92
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-09-08 13:29:22 +0200

    Add TasksList component and fix TasksListContainer

---
+++ src/components/TasksList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TasksList = () => {
+	return (
+		<div>
+			<ul className="tasks-list" />
+		</div>
+	);
+};
+
+export default TasksList;
--- src/components/TasksListContainer.js
+++ src/components/TasksListContainer.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import TasksList from './TasksList';
 
 const TasksListContainer = () => {
-	return <div className="tasks-list-container" />;
+	return (
+		<div className="tasks-list-container">
+			<h2 className="tasks-list__title">
+				lista zadań{' '}
+				<strong className="task-list__title--yellow">do zrobienia</strong>
+			</h2>
+			<TasksList />
+		</div>
+	);
 };
 
 export default TasksListContainer;
