19f156ec80bdb296a7b6e3fb5be6d12e34cdf8b9
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-09-08 13:20:31 +0200

    Add styles to TasksListContainer component

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AddTask from './AddTask';
+import TasksListContainer from './TasksListContainer';
 
 const TodoContainer = () => {
 	return (
 		<div className="todo-container">
 			<AddTask />
+			<TasksListContainer />
 		</div>
 	);
 };
