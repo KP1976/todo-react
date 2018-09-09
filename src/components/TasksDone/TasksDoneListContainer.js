9b5a908117869b2e670ca005f2312161a317ed2d
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-09-09 11:10:47 +0200

    Add styles to TasksDoneList component

--- src/components/TasksDone/TasksDoneListContainer.js
+++ src/components/TasksDone/TasksDoneListContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TasksDoneList from './TasksDoneList';
 
 const TasksDoneListContainer = () => {
 	return (
@@ -9,6 +10,7 @@ const TasksDoneListContainer = () => {
 					zrobionych
 				</strong>
 			</h2>
+			<TasksDoneList />
 		</div>
 	);
 };
