060774d207b6aabfec6ed33b88fa20617ef800a2
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-09-09 11:14:15 +0200

    Add TaskDone component

---
+++ src/components/TasksDone/TaskDone.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class TaskDone extends Component {
+	render() {
+		return (
+			<li className="task-done">
+				<p className="task-done__name-task">
+					Zrobić kilka projektów od tutoriala WesBosa
+				</p>
+				<p className="task-done__date-task">
+					Data wstawienia:{' '}
+					<span className="task-done__date-task-value">06.09.2018 r.</span>
+				</p>
+			</li>
+		);
+	}
+}
+
+export default TaskDone;
--- src/components/TasksDone/TasksDoneList.js
+++ src/components/TasksDone/TasksDoneList.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import TaskDone from './TaskDone';
 
 const TasksDoneList = () => {
-	return <ul className="tasks-done-list" />;
+	return (
+		<ul className="tasks-done-list">
+			<TaskDone />
+			<TaskDone />
+			<TaskDone />
+			<TaskDone />
+			<TaskDone />
+			<TaskDone />
+		</ul>
+	);
 };
 
 export default TasksDoneList;
