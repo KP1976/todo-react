804c3309909e0c058d1007dd73d28ee5e201c000
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-11-11 00:41:16 +0100

    Updates

---
+++ precache-manifest.1a1dc72dfd9ac32ad2b29b869ad2aa6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ebad8e7a9f5566e1598",
+    "url": "/todo-react/static/js/runtime~main.4ebad8e7.js"
+  },
+  {
+    "revision": "9671dcbe6ba3f9dbb05a",
+    "url": "/todo-react/static/js/main.9671dcbe.chunk.js"
+  },
+  {
+    "revision": "f724bdf4ff16d7bea9d8",
+    "url": "/todo-react/static/js/1.f724bdf4.chunk.js"
+  },
+  {
+    "revision": "9671dcbe6ba3f9dbb05a",
+    "url": "/todo-react/static/css/main.b30350a2.chunk.css"
+  },
+  {
+    "revision": "9658c6341f6b58fbfca2109a82b8619d",
+    "url": "/todo-react/index.html"
+  }
+];
